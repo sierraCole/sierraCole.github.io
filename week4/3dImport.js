5ac98051e60e7fff3147d6fbb18a2819b4b64d91
Sierra Cole <sac7820@nyu.edu>
2025-10-14 14:59:32 -0700

    start over

--- week4/3dImport.js
+++
@@ -1,6 +0,0 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.180.0";
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.180.0/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "https://cdn.skypack.dev/three@0.180.0/examples/jsm/loaders/GLTFLoader.js";
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
